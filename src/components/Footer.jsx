dcdfd3df86facce9e540ae7a3f7632f895b1682e
Sean Mafnas <seanvincent671@gmail.com>
2025-05-29 23:29:08 +1000

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; Copyright by ByteGrad. All Rights Reserved. </small>
+      <small>Last Checked Limits: 17 Days Ago</small>
+    </footer>
+  );
+}
---
+++ src/components/Textarea.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
+export default function Textarea({ text, setText }) {
+  const [warningText, setWarningText] = useState("");
+
+  const handleChange = (event) => {
+    let newText = event.target.value;
+    if (newText.includes("<script>")) {
+      setWarningText("No script tag allowed!");
+      newText = newText.replace("<script>", "");
+    } else if (newText.includes("@")) {
+      setWarningText("You cannot use the @ symbol!");
+      newText = newText.replace("@", "");
+    } else {
+      setWarningText("");
+    }
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
+      {warningText ? <Warning warningText={warningText} /> : null}
+    </div>
+  );
+}
